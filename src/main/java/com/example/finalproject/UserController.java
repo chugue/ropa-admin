f1c9404d6352a090418a38446d5535e8b7a43e9e
Stephen_acts <chugue85@gmail.com>
2024-04-19 18:16:38 +0900

    start

--- src/main/java/com/example/finalproject/UserController.java
+++ src/main/java/com/example/finalproject/UserController.java
@@ -1,17 +1,23 @@
 package com.example.finalproject;
 
 import jakarta.persistence.EntityManager;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class UserController {
-    private final EntityManager em;
+    private final HttpServletRequest request;
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        String file = "resources/static/img_file/cloth.png";
+        request.setAttribute("imgFilename", file);
         return "index";
     }
+
+
 }

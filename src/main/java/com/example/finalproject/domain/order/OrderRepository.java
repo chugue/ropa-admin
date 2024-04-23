a0fa0701d857c660d6ae54fd9f9dfa134e0bed29
AI_Nomads <chugue85@gmail.com>
2024-04-23 21:52:02 +0900

    toPull

--- src/main/java/com/example/finalproject/domain/order/OrderRepository.java
+++ src/main/java/com/example/finalproject/domain/order/OrderRepository.java
@@ -1,7 +1,7 @@
-package com.example.finalproject.domain.order;
-
-import com.example.finalproject.domain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
+//package com.example.finalproject.domain.order;
+//
+//import com.example.finalproject.domain.user.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface OrderRepository extends JpaRepository<Order, Integer> {
+//}

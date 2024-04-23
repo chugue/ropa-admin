aa6e746da240e17004b60de5135bd2c75df21b39
yeeun0118s <yeeun0118s@naver.com>
2024-04-23 17:38:22 +0900

    User 엔티티 구현 완료

--- src/main/java/com/example/finalproject/domain/user/User.java
+++ src/main/java/com/example/finalproject/domain/user/User.java
@@ -1,4 +1,30 @@
 package com.example.finalproject.domain.user;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "user_tb")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String myName; //실명
+    private String nickName; //별명
+    private String height; //키
+    private String weight; //체중
+    private String address; //주소
+    private String mobile; // 전화번호
+    private String email; // 아이디
+    private String password; //비밀번호
+    private Boolean blueChecked; //true -> 크리에이터, false -> 일반 회원
+    private Timestamp updateAt;
+    private Timestamp createdAt;
+
 }

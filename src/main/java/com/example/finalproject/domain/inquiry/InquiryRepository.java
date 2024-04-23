517ef46c9e07924597493ee5c0975bfd788a9579
Stephen_acts <chugue85@gmail.com>
2024-04-23 17:14:08 +0900

    setting

--- src/main/java/com/example/finalproject/domain/inquiry/InquiryRepository.java
+++ src/main/java/com/example/finalproject/domain/inquiry/InquiryRepository.java
@@ -1,6 +1,6 @@
-package com.example.finalproject.domain.inquiry;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InquiryRepository extends JpaRepository<Inquiry, Integer> {
-}
+//package com.example.finalproject.domain.inquiry;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface InquiryRepository extends JpaRepository<Inquiry, Integer> {
+//}

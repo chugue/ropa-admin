c47733ab5bbd95d38b66cd9d4f00ca625b43e0fe
yeeun0118s <yeeun0118s@naver.com>
2024-04-23 18:12:41 +0900

    회원가입 DTO 구현중

--- src/main/java/com/example/finalproject/domain/user/User.java
+++ src/main/java/com/example/finalproject/domain/user/User.java
@@ -1,6 +1,7 @@
 package com.example.finalproject.domain.user;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -24,7 +25,22 @@ public class User {
     private String email; // 아이디
     private String password; //비밀번호
     private Boolean blueChecked; //true -> 크리에이터, false -> 일반 회원
-    private Timestamp updateAt;
-    private Timestamp createdAt;
+    private Timestamp updateAt; // 회원 수정 시간
+    private Timestamp createdAt; //가입시간
 
+    @Builder
+    public User(Integer id, String myName, String nickName, String height, String weight, String address, String mobile, String email, String password, Boolean blueChecked, Timestamp updateAt, Timestamp createdAt) {
+        this.id = id;
+        this.myName = myName;
+        this.nickName = nickName;
+        this.height = height;
+        this.weight = weight;
+        this.address = address;
+        this.mobile = mobile;
+        this.email = email;
+        this.password = password;
+        this.blueChecked = blueChecked;
+        this.updateAt = updateAt;
+        this.createdAt = createdAt;
+    }
 }

196cef279065739323a01c434061885a7c372536
yeeun0118s <yeeun0118s@naver.com>
2024-04-25 16:16:33 +0900

    로그인 구현 완료

--- src/main/java/com/example/finalproject/domain/admin/AdminController.java
+++ src/main/java/com/example/finalproject/domain/admin/AdminController.java
@@ -14,12 +14,15 @@ public class AdminController {
     private final AdminService adminService;
     private final HttpSession session;
 
+    //로그인
     @PostMapping("login")
    public String login(AdminRequest.LoginDTO reqDTO, HttpServletRequest req){
         Admin admin = adminService.login(reqDTO);
         req.setAttribute("Admin", admin);
         return "index";
     }
+
+    //회원가입 관리자/브랜드
     @PostMapping("/join")
     public String join(AdminRequest.JoinDTO reqDTO, HttpServletRequest req){
         Admin admin = adminService.join(reqDTO);

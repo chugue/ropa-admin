ba417d55d14a2bcddd8416c505326e78b5610675
yeeun0118s <yeeun0118s@naver.com>
2024-04-27 22:40:09 +0900

    관리자 매출 관리 service 구현 완료

--- src/main/java/com/example/finalproject/domain/orderHistory/OrderHistoryService.java
+++ src/main/java/com/example/finalproject/domain/orderHistory/OrderHistoryService.java
@@ -1,5 +1,6 @@
 package com.example.finalproject.domain.orderHistory;
 
+import com.example.finalproject._core.error.exception.Exception404;
 import com.example.finalproject.domain.admin.Admin;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,18 +13,32 @@ public class OrderHistoryService {
 
     private final OrderHistoryRepository orderHistoryRepository;
 
+    //관리자 뷰 매출 목록 보기
+    private List<OrderHistory> adminOrderHistoryList(){
+
+      List<OrderHistory> adminOrderHistoryList = orderHistoryRepository.findAll();
+
+      if(adminOrderHistoryList == null){
+          throw  new Exception404("현재 주문내역이 존재 하지 않습니다.");
+
+      }
+
+      return adminOrderHistoryList;
+    }
+
+
 
     //브랜드 별 매출 목록보기
-    private List<OrderHistory> orderHistoryList(Integer adminId){
-
-        //1. 브랜드 별  매출 가져오기
-        List<OrderHistory> orderHistoryList = orderHistoryRepository.findByAdminId(adminId);
-
-        //2. DTO에 담기
-        List<>
-
-
-
-        //2.
-    }
+//    private List<OrderHistory> orderHistoryList(Integer adminId){
+//
+//        //1. 브랜드 별  매출 가져오기
+//        List<OrderHistory> orderHistoryList = orderHistoryRepository.findByAdminId(adminId);
+//
+//        //2. DTO에 담기
+//        List<>
+//
+//
+//
+//        //2.
+//    }
 }

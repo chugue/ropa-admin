d788395af5f9a462d095a2b5eec3a17ad61abd8a
yeeun0118s <yeeun0118s@naver.com>
2024-04-29 17:37:07 +0900

    응답 DTO 구현 완료

--- src/main/java/com/example/finalproject/domain/admin/AdminResponse.java
+++ src/main/java/com/example/finalproject/domain/admin/AdminResponse.java
@@ -1,7 +1,23 @@
 package com.example.finalproject.domain.admin;
 
+import com.example.finalproject.domain.orderHistory.OrderHistory;
 import lombok.Data;
 
 public class AdminResponse {
 
+    //브랜드의 매출 목록
+    @Data
+    public static class brandOrderHistoryListDTO {
+        private Integer itemsId;
+        private String itemsName;
+        private Integer totalPrice;
+        private Integer totalQuantity;
+
+        public brandOrderHistoryListDTO(OrderHistory orderHistory) {
+            this.itemsId = orderHistory.getItems().getId();
+            this.itemsName = orderHistory.getItems().getName();
+            this.totalPrice = orderHistory.getTotalPrice();
+            this.totalQuantity = orderHistory.getTotalQuantity();
+        }
+    }
 }

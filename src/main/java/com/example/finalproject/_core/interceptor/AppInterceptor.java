0351b9c9721fdd3c6beb476a4e08750480518c69
yeeun0118s <yeeun0118s@naver.com>
2024-05-02 22:18:04 +0900

    AppInterceptor 완료

--- src/main/java/com/example/finalproject/_core/interceptor/AppInterceptor.java
+++ src/main/java/com/example/finalproject/_core/interceptor/AppInterceptor.java
@@ -1,43 +1,43 @@
-//package com.example.finalproject._core.interceptor;
-//
-//import com.auth0.jwt.exceptions.JWTDecodeException;
-//import com.auth0.jwt.exceptions.TokenExpiredException;
-//import com.example.finalproject._core.error.exception.Exception401;
-//import com.example.finalproject._core.error.exception.Exception500;
-//import com.example.finalproject._core.utils.JwtUtill;
-//import com.example.finalproject.domain.user.SessionUser;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import jakarta.servlet.http.HttpSession;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//
-//public class AppInterceptor implements HandlerInterceptor {
-//
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        System.out.println("preHandle............");
-//
-//        String jwt = request.getHeader("Authorization");
-//        jwt = jwt.replace("Bearer ", "");
-//        HttpSession session = request.getSession();
-//
-//        try {
-//            SessionUser sessionUser = JwtUtill.verify(jwt);
-//            if (sessionAdmin.getRole().equals("ADMIN")) {
-//                session.setAttribute("sessionAdmin", sessionAdmin);
-//            }
-//            if (sessionAdmin.getRole().equals("BRAND")) {
-//                session.setAttribute("sessionBrand", sessionAdmin);
-//            }
-//        } catch (TokenExpiredException e) {
-//            throw new Exception401("토큰 만료 시간이 지났습니다. 다시 로그인 하세요");
-//        } catch (JWTDecodeException e) {
-//            throw new Exception401("토큰이 유효하지 않습니다.");
-//        } catch (Exception e) {
-//            e.printStackTrace(); // 개발 진행 시 TEST 보기
-//            throw new Exception500(e.getMessage()); //알수 없는 오류는 다 500으로 던진다.
-//        }
-//        return true;
-//    }
-//
-//}
+package com.example.finalproject._core.interceptor;
+
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.example.finalproject._core.error.exception.Exception401;
+import com.example.finalproject._core.error.exception.Exception500;
+import com.example.finalproject._core.utils.AppJwtUtill;
+import com.example.finalproject.domain.user.SessionUser;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AppInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandle............");
+
+        String jwt = request.getHeader("Authorization");
+        jwt = jwt.replace("Bearer ", "");
+        HttpSession session = request.getSession();
+
+        try {
+            SessionUser sessionUser = AppJwtUtill.verify(jwt);
+            if (sessionUser.getBlueChecked() == true) {
+                session.setAttribute("sessionCreator", sessionUser);
+            }
+            if (sessionUser.getBlueChecked() == false) {
+                session.setAttribute("sessionUser", sessionUser);
+            }
+        } catch (TokenExpiredException e) {
+            throw new Exception401("토큰 만료 시간이 지났습니다. 다시 로그인 하세요");
+        } catch (JWTDecodeException e) {
+            throw new Exception401("토큰이 유효하지 않습니다.");
+        } catch (Exception e) {
+            e.printStackTrace(); // 개발 진행 시 TEST 보기
+            throw new Exception500(e.getMessage()); //알수 없는 오류는 다 500으로 던진다.
+        }
+        return true;
+    }
+
+}

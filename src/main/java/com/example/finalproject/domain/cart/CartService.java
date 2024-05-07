d52798b118c93eb25dd58376958c3006ef0f9beb
tkffkels93 <tkffkels93@gmail.com>
2024-05-07 17:46:43 +0900

    사용자 장바구니 추가 및 수정 구현

--- src/main/java/com/example/finalproject/domain/cart/CartService.java
+++ src/main/java/com/example/finalproject/domain/cart/CartService.java
@@ -1,5 +1,8 @@
 package com.example.finalproject.domain.cart;
 
+import com.example.finalproject.domain.items.Items;
+import com.example.finalproject.domain.items.ItemsRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +13,7 @@ import java.util.stream.Collectors;
 @Service
 public class CartService {
     private final CartRepository cartRepository;
+    private final ItemsRepository itemsRepository;
 
     // 사용자 장바구니 목록 보기
     public CartResponse.CartDTO getCartByUserId(Integer userId) {
@@ -18,5 +22,28 @@ public class CartService {
                 .map(CartResponse.CartListDTO::new).collect(Collectors.toList());
         Integer totalCartPrice = cartListDTO.stream().mapToInt(CartResponse.CartListDTO::getTotalItemPrice).sum();
         return new CartResponse.CartDTO(userId, cartListDTO, totalCartPrice);
+    }
+
+    // 사용자 장바구니 추가
+    @Transactional
+    public void save(CartRequest.SaveDTO saveDTO, Integer userId, Integer itemId) {
+        // 아이템 ID를 기반으로 아이템 조회
+        Items items = itemsRepository.findById(itemId).orElse(null);
+
+        if (items != null) {
+            // 이미 장바구니에 있는지 확인
+            Cart IsCartItem = cartRepository.findByUserAndItem(userId, itemId);
+            if (IsCartItem != null) {
+                // 수량 증가
+                IsCartItem.setQuantity(saveDTO.getQuantity());
+                // 총 금액 업데이트
+                IsCartItem.setTotalAmount((items.getPrice() * saveDTO.getQuantity()));
+                // 장바구니에 있는 아이템 업데이트
+                cartRepository.save(IsCartItem);
+            } else {
+                // 새로운 아이템을 장바구니에 추가
+                cartRepository.save(saveDTO.toEntity());
+            }
+        }
     }
 }

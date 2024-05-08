455920f7e686c9ebfe1e4a7f15257868ecac02c4
tkffkels93 <tkffkels93@gmail.com>
2024-05-08 16:06:09 +0900

    주문하기 구현(배송지 제외)

--- src/main/java/com/example/finalproject/domain/order/OrderRequest.java
+++ src/main/java/com/example/finalproject/domain/order/OrderRequest.java
@@ -1,5 +1,37 @@
 package com.example.finalproject.domain.order;
 
+import com.example.finalproject.domain.cart.Cart;
+import com.example.finalproject.domain.user.User;
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.util.List;
+
 public class OrderRequest {
 
+    // 주문하기
+    @Data
+    public static class saveDTO {
+        private User user;
+        private List<Cart> cartItems;
+
+        public saveDTO(User user, List<Cart> cartItems) {
+            this.user = user;
+            this.cartItems = cartItems;
+        }
+
+        public static Order toEntity(User user, List<Cart> cartItems) {
+            int totalOrderAmount = 0;
+            for (Cart cartItem : cartItems) {
+                totalOrderAmount += cartItem.getTotalAmount();
+            }
+
+            // 주문 생성
+            return Order.builder()
+                    .user(user)
+                    .totalOrderAmount(totalOrderAmount)
+                    .orderDate(new Timestamp(System.currentTimeMillis()))
+                    .build();
+        }
+    }
 }
--- src/main/java/com/example/finalproject/domain/order/OrderService.java
+++ src/main/java/com/example/finalproject/domain/order/OrderService.java
@@ -1,10 +1,57 @@
 package com.example.finalproject.domain.order;
 
+import com.example.finalproject._core.error.exception.Exception401;
+import com.example.finalproject.domain.cart.Cart;
+import com.example.finalproject.domain.cart.CartRepository;
+import com.example.finalproject.domain.orderHistory.OrderHistory;
+import com.example.finalproject.domain.orderHistory.OrderHistoryRepository;
+import com.example.finalproject.domain.user.User;
+import com.example.finalproject.domain.user.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final CartRepository cartRepository;
+    private final UserRepository userRepository;
+    private final OrderHistoryRepository orderHistoryRepository;
+
+    // 주문하기
+    @Transactional
+    public Order saveOrder(Integer userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new Exception401("사용자의의 정보를 찾을 수 없습니다."));
+        // 사용자의 장바구니에서 아이템들을 가져옴
+        List<Cart> cartItems = cartRepository.findAllByUserId(userId);
+
+        // 주문 생성
+        Order order = OrderRequest.saveDTO.toEntity(user, cartItems);
+
+        // 장바구니에서 주문내역으로 이동
+        for (Cart cartItem : cartItems) {
+            OrderHistory orderHistory = OrderHistory.builder()
+                    .admin(cartItem.getItems().getAdmin())
+                    .order(order)
+                    .items(cartItem.getItems())
+                    .orderItemQty(cartItem.getQuantity())
+                    .orderItemPrice(cartItem.getTotalAmount())
+                    .build();
+
+            // 주문 내역 저장
+            orderHistoryRepository.save(orderHistory);
+        }
+
+        // 주문 저장
+        orderRepository.save(order);
+
+        // 장바구니 비우기
+        cartRepository.deleteAll(cartItems);
+
+        return order;
+    }
 }

ac194a10280158ff560672064a89b79544798061
AI_Nomads <chugue85@gmail.com>
2024-05-09 11:05:42 +0900

    결제 테이블 생성

--- src/main/java/com/example/finalproject/domain/purchase/DeliveryAddress.java
+++ src/main/java/com/example/finalproject/domain/purchase/DeliveryAddress.java
@@ -1,18 +1,13 @@
 package com.example.finalproject.domain.purchase;
 
 
-import com.example.finalproject.domain.order.Order;
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@Entity
-@Table(name = "puchase_tb")
-@Data
-public class Purchase {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-}
+//@NoArgsConstructor
+//@Entity
+//@Table(name = "puchase_tb")
+//@Data
+//public class Purchase {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Integer id;
+//
+//}

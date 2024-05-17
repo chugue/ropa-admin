ebea754da51f6a39d3e66705d75504b205b87ade
Stephen_acts <chugue85@gmail.com>
2024-05-17 18:19:22 +0900

    done

--- src/test/java/com/example/finalproject/user/UserControllerTest.java
+++ src/test/java/com/example/finalproject/user/UserControllerTest.java
@@ -2,22 +2,56 @@ package com.example.finalproject.user;
 
 
 import com.example.finalproject.domain.user.UserRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.internal.constraintvalidators.bv.number.sign.PositiveValidatorForBigDecimal;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class UserControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    private ObjectMapper om = new ObjectMapper();
+    private static String jwt;
+
 
 
     @Test
-    public void join_test(){
+    public void login_success_test() throws Exception {
         // given
-        UserRequest.JoinDTO reqDTO = new UserRequest.JoinDTO();
+        UserRequest.LoginDTO reqDTO = new UserRequest.LoginDTO();
+        reqDTO.setEmail("junghein@example.com");
+        reqDTO.setPassword("1234");
+
+        String reqBody = om.writeValueAsString(reqDTO);
         // when
-
+        ResultActions actions = mvc.perform(
+                post("/user/login")
+                        .content(reqBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
         // eye
+        String respBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println("respBody = " + respBody);
 
+        String jwt = actions.andReturn().getResponse().getHeader("Authorization");
+        System.out.println("jwt = " + jwt);
         // then
-
+//        actions.andExpect(status().isOk());
+//        actions.andExpect(result -> result.getResponse().getHeader("Authorization").contains("Bearer " + jwt));
+//        actions.andExpect()
     }
 }
